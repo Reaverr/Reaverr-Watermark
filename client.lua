7a88031d759b8590dca8ac048042a5c0fff53c79
Reaverr <57047754+Reaverr@users.noreply.github.com>
2019-11-06 18:34:31 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,11 +1,13 @@
+--[Coded By Reaverr]
+
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(1)
-        SetTextFont(4) -- 0-4 פונט
-        SetTextScale(0.4, 0.4) -- גודל הטקסט
-        SetTextColour(255, 0, 0, 0.8) -- צבע (r, g, b, a)
+        SetTextFont(4) --[פונט מ1 עד 10]
+        SetTextScale(0.4, 0.4) --[גודל הטקסט]
+        SetTextColour(255, 0, 0, 0.8) --[הצבע של הטקסט-(r, g, b, a)]
         SetTextEntry("STRING")
-        AddTextComponentString("Enter Text Here!") -- טקסט על המסך
-        DrawText(0.0040, 0.0001) -- מיקום על המסך
+        AddTextComponentString("Enter Text Here!") --[הטקסט שיופיעה על המסך]
+        DrawText(0.0040, 0.0001) --[המיקום של הטקסט על המסך]
     end
 end)
