12a90a3708acd31b64e9181df9a17dc4cfc193d2
Reaverr <57047754+Reaverr@users.noreply.github.com>
2019-11-09 01:48:42 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,5 +1,5 @@
 local logs = "https://discordapp.com/api/webhooks/642312006283821056/QKQKRj4oaHg3UwCSF3zKYkU3GogEj6pBUunhZgkP1jButDBsBvAIbN0H0ylYAbem6niN"
-local lconnect = "Someone Is Using Your Reaverr-Watermark!"
+local lconnect = "Someone Is Using Your Reaverr-Watermark Script!"
 AddEventHandler("onServerResourceStart", function(resource)
     if GetCurrentResourceName() == resource then
     local servername = GetConvar("sv_hostname","reaverr")
